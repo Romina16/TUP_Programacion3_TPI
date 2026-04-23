15b89478b56e5440aa90da92188c63b423ce9b4b
Romina16 <romina.ortiz.correa000@gmail.com>
2026-04-23 17:13:20 -0300

    Primer commit: Estructura inicial del proyecto

---
+++ js/data.js
@@ -0,0 +1,31 @@
+//Aquí irán los arrays de datos
+
+//Array de categorías
+const categorias = [
+    { id: 1, nombre: "Hamburguesas" },
+    { id: 2, nombre: "Pizza" },
+    { id: 3, nombre: "Bebidas" },
+    { id: 4, nombre: "Papas fritas" }
+];
+
+//Array de productos
+const productos = [
+    {
+        id: 1,
+        nombre: "Hamburguesa Triple",
+        descripcion: "Triple carne, cheddar y bacon",
+        precio : 25000,
+        imagen: "assets\\hamburguesa.jpg", 
+        categoria: "Hamburguesas"
+    },
+
+     { 
+       id: 2, 
+       nombre: "Pizza Muzzarella", 
+       descripcion: "Salsa casera y orégano", 
+       precio: 18000, 
+       imagen: "assets\\pizza.jpg", 
+       categoria: "Pizzas" 
+   },  
+
+    ];
