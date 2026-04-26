e40608ecfe4fc27254b848e208862830207591d7
Romina16 <romina.ortiz.correa000@gmail.com>
2026-04-26 17:20:14 -0300

    Se agrego logica con js y estilos a index

--- js/data.js
+++ js/data.js
@@ -14,18 +14,32 @@ const productos = [
         id: 1,
         nombre: "Hamburguesa Triple",
         descripcion: "Triple carne, cheddar y bacon",
-        precio : 25000,
-        imagen: "assets\\hamburguesa.jpg", 
+        precio: 25000,
+        imagen: "assets\\hamburguesa.jpg",
         categoria: "Hamburguesas"
     },
-
-     { 
-       id: 2, 
-       nombre: "Pizza Muzzarella", 
-       descripcion: "Salsa casera y orégano", 
-       precio: 18000, 
-       imagen: "assets\\pizza.jpg", 
-       categoria: "Pizzas" 
-   },  
-
-    ];
+    {
+        id: 2,
+        nombre: "Pizza Muzzarella",
+        descripcion: "Salsa casera y orégano",
+        precio: 18000,
+        imagen: "assets\\pizza.jpg",
+        categoria: "Pizzas"
+    },
+    {
+        id: 3,
+        nombre: "Papas Rústicas",
+        descripcion: "Condimento especial",
+        precio: 8000,
+        imagen: "assets/papasrusticas.jpg",
+        categoria: "Papas fritas"
+    },
+    {
+        id: 4,
+        nombre: "Cerveza Artesanal",
+        descripcion: "Ipa de la casa 500ml",
+        precio: 6000,
+        imagen: "assets/Pinta.jpg",
+        categoria: "Bebidas"
+    }
+];
--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 //Aquí irá la lógica de renderizado
+
+const cargarCategorias = () => {
+    const contenedor = document.getElementById("lista-categorias");
+    categorias.forEach(cat => {
+
+        const li = document.createElement("li");
+        li.innerHTML = `<a href="#">${cat.nombre}</a>`;
+        contenedor.appendChild(li);
+    });
+}
+
+const cargarProductos = () => {
+
+    const contenedorProductos = document.getElementById("contenedor-productos");
+
+    productos.forEach(producto => {
+
+        const article = document.createElement("article"); //creacion de nodo article
+
+        article.classList.add("tarjeta-producto") //aplico estilo de tarjeta-producto
+
+        article.innerHTML = `
+        <img src= "${producto.imagen}" alt ="${producto.nombre}">
+        <h3>${producto.nombre}</h3>
+        <p>${producto.descripcion}</p>
+        <p class ="precio">$${producto.precio}</p>
+        <button class="btn-agregar">Agregar</button>
+        `;
+
+        const boton = article.querySelector(".btn-agregar");
+        boton.addEventListener("click", () => {
+            alert(`Seleccionaste: ${producto.nombre}`);
+        })
+
+        contenedorProductos.appendChild(article);
+    })
+}
+
+
+//Mostrar el contenido
+cargarCategorias();
+cargarProductos();
